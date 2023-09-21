48d95026d36f2efbf81c6cb733f5329ea29fe6af
justnazarko <justnazarko@gmail.com>
2023-09-21 22:04:26 +0300

    moved validation in separate function

--- nazarii_tereshkevych_pmi25/task1/main.py
+++ nazarii_tereshkevych_pmi25/task1/main.py
@@ -1,32 +1,27 @@
+def validate_input(prompt):
+    while True:
+        try:
+            value = int(input(prompt))
+            if value < 1:
+                print("Будь ласка, введіть ціле число більше або рівне 1.")
+            else:
+                return value
+        except ValueError:
+            print("Будь ласка, введіть ціле число більше або рівне 1.")
+
 def count_ways_to_top(K, N):
-
-
-
     ways = [0] * (N + 1)
-
-
     ways[0] = 1
-
 
     for i in range(1, N + 1):
         for j in range(1, min(K, i) + 1):
             ways[i] += ways[i - j]
 
-
     return ways[N]
 
 
-
-while True:
-    try:
-        K = int(input("Введіть максимальну кількість сходинок, які може подолати заєць (K): "))
-        N = int(input("Введіть кількість сходинок (N): "))
-        if K < 1 or N < 1:
-            print("Будь ласка, введіть цілі невід'ємні числа для K та N.")
-        else: break
-    except ValueError:
-        print("Будь ласка, введіть цілі невід'ємні числа для K та N.")
-
+        K = validate_input("Введіть максимальну кількість сходинок, які може подолати заєць (K): ")
+        N = validate_input("Введіть кількість сходинок (N): ")
 
 result = count_ways_to_top(K, N)
 print(f"Кількість різних способів дістатися до вершини сходів: {result}")
